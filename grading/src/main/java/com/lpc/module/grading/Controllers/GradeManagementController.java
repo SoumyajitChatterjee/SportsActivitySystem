5b8b81424707d2af1b22cbc7dbce7fbf14fc6bc8
Joyjit2001 <Chatwithjoyjit@gmail.com>
2023-01-01 09:08:11 -0800

    Add API implementation and backend sql connection

--- grading/src/main/java/com/lpc/module/grading/Controllers/GradeManagementController.java
+++ grading/src/main/java/com/lpc/module/grading/Controllers/GradeManagementController.java
@@ -1,33 +1,64 @@
 package com.lpc.module.grading.Controllers;
 
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lpc.module.grading.Models.Grade;
+import com.lpc.module.grading.Models.UserRepository;
 
 @RestController
 public class GradeManagementController {
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/grades")
     public String createGrade(@RequestBody Grade grade) {
+        userRepository.save(grade);
         return  grade.getGrade();
     }
     @GetMapping("/grades/{id}")
-    public String retreiveGrade(@PathVariable(name="id") int gradeId) {
-        return new String("Grade Successfully Entered");
+    public @ResponseBody Optional<Grade> retreiveGrade(@PathVariable(name="id") int gradeId) {
+        return userRepository.findById(gradeId);
+        // return new String("Grade Successfully Entered");
+    }
+    @GetMapping("/grades")
+    public @ResponseBody Iterable<Grade> retreiveAllGrade() {
+        return userRepository.findAll();
+        // return new String("Grade Successfully Entered");
     }
     @DeleteMapping("/grades/{id}")
     public String deleteGrade(@PathVariable(name="id") int gradeId) {
+        userRepository.deleteById(gradeId);
         return new String("Grade Successfully Deleted");
     }
+
     @PutMapping("/grades/{id}")
-    public String createGrade(@PathVariable(name="id") int gradeId, @RequestBody Grade grade) {
-        return new String("Grade Successfully Updated");
+    public @ResponseBody Optional<Grade> createGrade(@PathVariable(name="id") int justId, @RequestBody Grade grade) {
+        Grade retGrade = userRepository.findById(justId).get();
+        BeanUtils.copyProperties(grade, retGrade);
+        retGrade.setId(justId);
+        userRepository.save(retGrade);
+        return userRepository.findById(justId);
+    }
+
+    @PatchMapping("/grades/{id}")
+    public @ResponseBody Optional<Grade> createGrade(@PathVariable(name="id") int gradeId, @RequestBody String grade) {
+        Grade retGrade = userRepository.findById(gradeId).get();
+        retGrade.setGrade(grade);
+        userRepository.save(retGrade);
+        return userRepository.findById(gradeId);
     }
 
 }
--- grading/src/main/java/com/lpc/module/grading/Models/Grade.java
+++ grading/src/main/java/com/lpc/module/grading/Models/Grade.java
@@ -1,14 +1,23 @@
 package com.lpc.module.grading.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class Grade {
-    private String grade;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
 
+    private String grade;
 }
